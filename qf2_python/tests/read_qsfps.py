5e492d4fa93370f3c78c86070c31525c3bd758d6
John Jones <jxj@jxj.me.uk>
2020-01-31 18:29:32 -0800

    Added configuration functions to help with editing settings at runtime. Removed disable/enable main power scripts. Added toggle_setting script instead, as more generic. read_qsfps moved to tests, currently not functional.

---
+++ qf2_python/tests/read_qsfps.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import argparse
+import qf2_python.identifier as identifier
+
+parser = argparse.ArgumentParser(description='Identify a board and get an interface', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('-t', '--target', default='192.168.1.127', help='Target IP address')
+parser.add_argument('-v', '--verbose', action="store_true", help='Verbose output')
+args = parser.parse_args()
+
+# Only in the runtime
+identifier.verifyInRuntime(args.target, args.verbose)
+
+x = identifier.get_active_interface(args.target, args.verbose)
+
+v = x.spartan_qsfp_get()
+
+for key, value in v.iteritems():
+    print(key, ':', value)
+    
+v = x.kintex_qsfp_1_get()
+
+for key, value in v.iteritems():
+    print(key, ':', value)
+
+v = x.kintex_qsfp_2_get()
+
+for key, value in v.iteritems():
+    print(key, ':', value)
+
