31041b0eb71620d9f79b3d1fe9a2aa1f55f27f32
John Jones <jxj@jxj.me.uk>
2020-01-31 17:52:57 -0800

    Modifications to bootloader to support python3.

--- qf2_python/scripts/read_qsfps.py
+++ qf2_python/scripts/read_qsfps.py
@@ -1,22 +1,25 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import argparse
-import qf2_python.identifier
+import qf2_python.identifier as identifier
 
 parser = argparse.ArgumentParser(description='Identify a board and get an interface', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-t', '--target', default='192.168.1.127', help='Target IP address')
 parser.add_argument('-v', '--verbose', action="store_true", help='Verbose output')
 args = parser.parse_args()
 
-x = qf2_python.identifier.get_active_interface(args.target, args.verbose)
+# Only in the runtime
+identifier.verifyInRuntime(args.target, args.verbose)
+
+x = identifier.get_active_interface(args.target, args.verbose)
 
 v = x.kintex_qsfp_1_get()
 
 for key, value in v.iteritems():
-    print key, ':', value
+    print(key, ':', value)
 
 v = x.kintex_qsfp_2_get()
 
 for key, value in v.iteritems():
-    print key, ':', value
+    print(key, ':', value)
 
